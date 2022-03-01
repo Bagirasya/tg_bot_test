e889f7ace886cc8664118f3c85b3e115bc14b6ea
asya <asya.gailamazyan@yandex.ru>
2022-03-01 16:49:39 +0300

    start handling

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import telebot
 
 bot = telebot.TeleBot(config.TOKEN)
 
+@bot.message_handler(commands=['start'])
+def boo(message):
+    bot.send_message(
+        message.chat.id, "Hi, {0.first_name}.\nGive me your birth date, time and place, please".format(message.from_user), parse_mode='html')
+
 @bot.message_handler(content_types=['text'])
 def boo(message):
     bot.send_message(message.chat.id, message.text)
